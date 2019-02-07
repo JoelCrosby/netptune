0eac47ca7f01eee05ce41917525b71e7a16705d8
Joel Crosby <JoelCrosby@live.co.uk>
2019-02-07 00:57:17 +0000

    add username to appuser interface

--- Netptune.App/ClientApp/src/app/models/appuser.ts
+++ Netptune.App/ClientApp/src/app/models/appuser.ts
@@ -2,14 +2,14 @@ import { ProjectTask } from './project-task';
 
 export interface AppUser {
 
-    id: string;
+  id: string;
 
-    firstName: string;
-    lastName: string;
-    email: string;
-    pictureUrl: string;
-    lastLoginTime: Date;
-    registrationDate: Date;
-    tasks: ProjectTask[];
-
+  firstName: string;
+  lastName: string;
+  email: string;
+  userName: string
+  pictureUrl: string;
+  lastLoginTime: Date;
+  registrationDate: Date;
+  tasks: ProjectTask[];
 }
