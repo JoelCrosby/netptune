e6be66321b5394d7e6db259cd7358dbea4a81ed4
Joel Crosby <JoelCrosby@live.co.uk>
2019-04-09 18:48:29 +0100

    remove task status classes

--- Netptune.App/ClientApp/src/app/features/project-tasks/components/task-list-item/task-list-item.component.ts
+++ Netptune.App/ClientApp/src/app/features/project-tasks/components/task-list-item/task-list-item.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { MatDialog, MatSnackBar } from '@angular/material';
 import { toggleChip } from '@app/core/animations/animations';
 import { ProjectTaskDto } from '@app/core/models/view-models/project-task-dto';
-import { ProjectTaskStatus } from '@app/core/enums/project-task-status';
 
 @Component({
   selector: 'app-task-list-item',
@@ -12,19 +10,4 @@ import { ProjectTaskStatus } from '@app/core/enums/project-task-status';
 })
 export class TaskListItemComponent {
   @Input() task: ProjectTaskDto;
-
-  constructor(public dialog: MatDialog, public snackBar: MatSnackBar) {}
-
-  getStatusClass(task: ProjectTaskDto): string {
-    switch (task.status) {
-      case ProjectTaskStatus.Complete:
-        return 'fas fa-check completed';
-      case ProjectTaskStatus.InProgress:
-        return 'fas fa-minus in-progress';
-      case ProjectTaskStatus.OnHold:
-        return 'fas fa-minus-circle blocked';
-      default:
-        return 'fas fa-stream none';
-    }
-  }
 }
