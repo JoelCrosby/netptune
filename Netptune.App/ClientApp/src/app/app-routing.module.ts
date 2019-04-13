6e8e1284318695bf363323f542144590f90199f6
Joel Crosby <JoelCrosby@live.co.uk>
2019-04-14 00:26:17 +0100

    preload feature modules

--- Netptune.App/ClientApp/src/app/app-routing.module.ts
+++ Netptune.App/ClientApp/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { AuthGuardService } from './core/auth/auth-guard.service';
 
@@ -64,7 +64,13 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload', enableTracing: false })],
+  imports: [
+    RouterModule.forRoot(routes, {
+      onSameUrlNavigation: 'reload',
+      enableTracing: false,
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
