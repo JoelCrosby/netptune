3718ac7df1fa35eef3d55209657527f7b71377f0
Joel Crosby <JoelCrosby@live.co.uk>
2019-04-15 21:45:07 +0100

    make projects use entities

--- Netptune.App/ClientApp/src/app/features/projects/store/projects.reducer.ts
+++ Netptune.App/ClientApp/src/app/features/projects/store/projects.reducer.ts
@@ -1,8 +1,9 @@
 import { ProjectsActions, ProjectsActionTypes } from './projects.actions';
 import { Project } from '@app/core/models/project';
+import { EntityAdapter, createEntityAdapter, EntityState } from '@ngrx/entity';
 
 export interface ProjectsState {
-  projects: Project[];
+  projects: Projects;
   loading: boolean;
   loaded: boolean;
   loadProjectsError?: any;
@@ -11,11 +12,15 @@ export interface ProjectsState {
 }
 
 export const initialState: ProjectsState = {
-  projects: [],
+  projects: { ids: [], entities: {} },
   loading: false,
   loaded: false,
   createProjectLoading: false,
 };
+
+export interface Projects extends EntityState<Project> {}
+
+export const adapter: EntityAdapter<Project> = createEntityAdapter<Project>();
 
 export function projectsReducer(state = initialState, action: ProjectsActions): ProjectsState {
   switch (action.type) {
@@ -24,7 +29,12 @@ export function projectsReducer(state = initialState, action: ProjectsActions): 
     case ProjectsActionTypes.LoadProjectsFail:
       return { ...state, loading: false, loadProjectsError: action.payload };
     case ProjectsActionTypes.LoadProjectsSuccess:
-      return { ...state, loading: false, loaded: true, projects: action.payload };
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        projects: adapter.addAll(action.payload, state.projects),
+      };
     case ProjectsActionTypes.CreateProject:
       return { ...state, createProjectLoading: true };
     case ProjectsActionTypes.CreateProjectFail:
@@ -33,9 +43,16 @@ export function projectsReducer(state = initialState, action: ProjectsActions): 
       return {
         ...state,
         createProjectLoading: false,
-        projects: [...state.projects, action.payload],
+        projects: adapter.addOne(action.payload, state.projects),
       };
     default:
       return state;
   }
 }
+
+const { selectIds, selectEntities, selectAll, selectTotal } = adapter.getSelectors();
+
+export const selectProjectIds = selectIds;
+export const selectProjectEntities = selectEntities;
+export const selectAllProjects = selectAll;
+export const selectUserTotal = selectTotal;
--- Netptune.App/ClientApp/src/app/features/projects/store/projects.selectors.ts
+++ Netptune.App/ClientApp/src/app/features/projects/store/projects.selectors.ts
@@ -1,12 +1,17 @@
 import { createSelector, createFeatureSelector } from '@ngrx/store';
-import { ProjectsState } from './projects.reducer';
+import { ProjectsState, selectAllProjects } from './projects.reducer';
 import { AppState } from '@app/core/core.state';
 
 export const selectProjectsFeature = createFeatureSelector<ProjectsState>('projects');
 
-export const selectProjects = createSelector(
+export const selectProjectsEntity = createSelector(
   selectProjectsFeature,
   (state: ProjectsState) => state.projects
+);
+
+export const selectProjects = createSelector(
+  selectProjectsEntity,
+  selectAllProjects
 );
 
 export const selectProjectsLoading = createSelector(
