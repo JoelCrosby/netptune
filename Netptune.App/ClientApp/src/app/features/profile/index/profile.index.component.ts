9b9d0c53654abe1929bb3184bc3a2084c9812542
Joel Crosby <JoelCrosby@live.co.uk>
2019-04-27 01:18:55 +0100

    add logut fuctionality to profile screen

--- Netptune.App/ClientApp/src/app/features/profile/index/profile.index.component.ts
+++ Netptune.App/ClientApp/src/app/features/profile/index/profile.index.component.ts
@@ -1,8 +1,15 @@
+import { ActionAuthLogout } from '@app/core/auth/store/auth.actions';
+import { AppState } from '@app/core/core.state';
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.index.component.html',
   styleUrls: ['./profile.index.component.scss'],
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  constructor(private store: Store<AppState>) {}
+
+  onLogoutClicked = () => this.store.dispatch(new ActionAuthLogout());
+}
