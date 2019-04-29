cd360e2a3b951e9729d9aad9002ef072cbd1ce9e
Joel Crosby <JoelCrosby@live.co.uk>
2019-04-30 00:12:45 +0100

    make tail customisable

--- Netptune.App/ClientApp/src/app/core/util/text-helpers.ts
+++ Netptune.App/ClientApp/src/app/core/util/text-helpers.ts
@@ -1,8 +1,9 @@
 export class TextHelpers {
+  static readonly tail = ' ...';
   static truncate(input: string, maxLength = 48) {
     if (input.length <= maxLength) {
       return input;
     }
-    return input.substring(0, maxLength - 4) + ' ...';
+    return input.substring(0, maxLength - this.tail.length) + this.tail;
   }
 }
