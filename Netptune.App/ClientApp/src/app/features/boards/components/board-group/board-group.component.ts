57f027c462841a944b46a85fa6d5226043165809
Joel Crosby <Joelcrosby@live.co.uk>
2020-01-07 21:23:19 +0000

    make add task button only visible on hover

--- Netptune.App/ClientApp/src/app/features/boards/components/board-group/board-group.component.ts
+++ Netptune.App/ClientApp/src/app/features/boards/components/board-group/board-group.component.ts
@@ -1,13 +1,50 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { BoardGroup } from '@app/core/models/board-group';
+import { BehaviorSubject, fromEvent, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-board-group',
   templateUrl: './board-group.component.html',
   styleUrls: ['./board-group.component.scss'],
 })
-export class BoardGroupComponent implements OnInit {
+export class BoardGroupComponent implements OnInit, OnDestroy, AfterViewInit {
   @Input() group: BoardGroup;
+  @ViewChild('container') container: ElementRef;
+
+  focusedSubject = new BehaviorSubject<boolean>(false);
+  onDestroy$ = new Subject();
+
+  focused$ = this.focusedSubject.pipe();
 
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    const el = this.container.nativeElement;
+
+    fromEvent(el, 'mouseenter', { passive: true })
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe({
+        next: () => this.focusedSubject.next(true),
+      });
+
+    fromEvent(el, 'mouseleave', { passive: true })
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe({
+        next: () => this.focusedSubject.next(false),
+      });
+  }
+
+  ngOnDestroy() {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
 }
