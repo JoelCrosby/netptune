629f009644871616ff3b4493782a9ea7f6a2a1e3
Joel Crosby <Joelcrosby@live.co.uk>
2020-05-14 03:27:00 +0100

    replace old avatars

---
+++ src/app/static/pipes/avatar.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'avatar',
+})
+export class AvatarPipe implements PipeTransform {
+  transform(value: unknown, ...args: unknown[]): unknown {
+    if (typeof value !== 'string') {
+      return null;
+    }
+
+    if (value.length < 1) return null;
+
+    const words = value.split(' ').map((val) => val.trim());
+
+    if (words.length === 1) return words[0][0];
+
+    return `${words[0][0]}${words[1][0]}`;
+  }
+}
