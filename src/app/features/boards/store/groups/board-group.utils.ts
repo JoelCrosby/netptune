d9f7d18cd892ce004d0189bd691c0a639ab97c1f
Joel Crosby <joelcrosby@live.co.uk>
2020-06-12 23:22:35 +0100

    update tasks status in front end when moving to another group

--- src/app/features/boards/store/groups/board-group.utils.ts
+++ src/app/features/boards/store/groups/board-group.utils.ts
@@ -2,27 +2,30 @@ import { BoardGroupsState } from './board-groups.model';
 import { MoveTaskInGroupRequest } from '@app/core/models/move-task-in-group-request';
 import { moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { getNewSortOrder } from '@app/core/util/sort-order-helper';
+import { getTaskStatusFromGroupType } from '@app/core/project-tasks/status-utils';
 
 export const moveTaskInBoardGroup = (
   state: BoardGroupsState,
   request: MoveTaskInGroupRequest
 ): BoardGroupsState => {
+  const newGroup = state.entities[request.newGroupId];
+
   if (request.oldGroupId === request.newGroupId) {
     moveItemInArray(
-      state.entities[request.newGroupId].tasks,
+      newGroup.tasks,
       request.previousIndex,
       request.currentIndex
     );
   } else {
     transferArrayItem(
       state.entities[request.oldGroupId].tasks,
-      state.entities[request.newGroupId].tasks,
+      newGroup.tasks,
       request.previousIndex,
       request.currentIndex
     );
   }
 
-  const tasks = state.entities[request.newGroupId].tasks;
+  const tasks = newGroup.tasks;
 
   const prevTask = tasks[request.currentIndex - 1];
   const nextTask = tasks[request.currentIndex + 1];
@@ -37,7 +40,11 @@ export const moveTaskInBoardGroup = (
   ].tasks.map((task) => {
     if (task.id !== request.taskId) return task;
 
-    return { ...task, sortOrder };
+    return {
+      ...task,
+      sortOrder,
+      status: getTaskStatusFromGroupType(newGroup.type),
+    };
   });
 
   return state;
