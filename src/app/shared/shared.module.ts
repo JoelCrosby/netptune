a2a1cc8daffbedd91ad535b05e5c98dae1e20de4
Joel Crosby <joelcrosby@live.co.uk>
2020-07-18 02:46:21 +0100

    move snackbar position

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -98,7 +98,13 @@ import {
     MatTreeModule,
   ],
   providers: [
-    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 2500 } },
+    {
+      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+      useValue: {
+        duration: 2500,
+        horizontalPosition: 'right',
+      },
+    },
     {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
       useValue: {
