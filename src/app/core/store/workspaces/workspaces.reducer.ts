946a3680f09ced92abfae0c14dd82b95c80000fd
Joel Crosby <joelcrosby@live.co.uk>
2020-07-20 02:46:20 +0100

    more comments

--- src/app/core/store/workspaces/workspaces.reducer.ts
+++ src/app/core/store/workspaces/workspaces.reducer.ts
@@ -4,7 +4,10 @@ import { adapter, initialState, WorkspacesState } from './workspaces.model';
 
 const reducer = createReducer(
   initialState,
-  on(actions.loadWorkspaces, state => ({ ...state, loading: true })),
+
+  // Load Workspaces
+
+  on(actions.loadWorkspaces, (state) => ({ ...state, loading: true })),
   on(actions.loadWorkspacesFail, (state, { error }) => ({
     ...state,
     loadingError: error,
@@ -12,7 +15,10 @@ const reducer = createReducer(
   on(actions.loadWorkspacesSuccess, (state, { workspaces }) =>
     adapter.setAll(workspaces, { ...state, loading: false, loaded: true })
   ),
-  on(actions.createWorkspace, state => ({ ...state, loadingCreate: true })),
+
+  // Create Workspaces
+
+  on(actions.createWorkspace, (state) => ({ ...state, loadingCreate: true })),
   on(actions.createWorkspaceFail, (state, { error }) => ({
     ...state,
     loadingError: error,
@@ -23,7 +29,10 @@ const reducer = createReducer(
       loadingCreate: false,
     })
   ),
-  on(actions.editWorkspace, state => ({ ...state, loadingEdit: true })),
+
+  // Edit Workspaces
+
+  on(actions.editWorkspace, (state) => ({ ...state, loadingEdit: true })),
   on(actions.editWorkspaceFail, (state, { error }) => ({
     ...state,
     editError: error,
@@ -34,10 +43,16 @@ const reducer = createReducer(
       loadingEdit: false,
     })
   ),
+
+  // Select Workspaces
+
   on(actions.selectWorkspace, (state, { workspace }) => ({
     ...state,
     currentWorkspace: workspace,
   })),
+
+  // Delete Workspace
+
   on(actions.deleteWorkspaceSuccess, (state, { workspace }) =>
     adapter.removeOne(workspace.id, state)
   )
