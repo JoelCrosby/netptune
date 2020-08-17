6226857ccc58ff1188cde6dce9235878abb47435
Joel Crosby <joelcrosby@live.co.uk>
2020-08-18 00:56:09 +0100

    add profiler in development mode

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, NgModuleRef, ApplicationRef } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from '@env/environment';
+import { enableDebugTools } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
+// Using Profile
+// Execute tyhe following in the devtools console:
+// > ng.profiler.timeChangeDetection()
+
+const addProfiling = (module: NgModuleRef<AppModule>): void => {
+  if (environment.production) return;
+
+  enableDebugTools(module.injector.get(ApplicationRef).components[0]);
+};
+
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
+  .then((module) => addProfiling(module))
   .catch((err) => console.log(err));
