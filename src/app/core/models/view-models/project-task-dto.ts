6eee2729315aee77241a04d5e4e34c42e9f340f8
Joel Crosby <joelcrosby@live.co.uk>
2020-08-18 20:15:00 +0100

    allow re-assigning tasks

--- src/app/core/models/view-models/project-task-dto.ts
+++ src/app/core/models/view-models/project-task-dto.ts
@@ -2,8 +2,9 @@ import { ProjectTask } from '../project-task';
 
 export interface TaskViewModel extends ProjectTask {
   assigneeUsername: string;
-  ownerUsername: string;
   assigneePictureUrl: string;
+  ownerUsername: string;
+  ownerPictureUrl: string;
   projectName: string;
   systemId: string;
   workspaceSlug: string;
