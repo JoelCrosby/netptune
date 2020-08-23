2ed8074ca4a5be4abb901e8457db3306cd7ed06e
Joel Crosby <joelcrosby@live.co.uk>
2020-08-23 03:02:04 +0100

    add gerenal list styles to use for users view

--- src/app/features/users/users.module.ts
+++ src/app/features/users/users.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@app/shared/shared.module';
 import { StaticModule } from '@app/static/static.module';
-import { UserCardComponent } from './components/user-card/user-card.component';
-import { UsersListComponent } from './components/users-list/users-list.component';
+import { UserListItemComponent } from './components/user-list-item/user-list-item.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersViewComponent } from './views/users-view/users-view.component';
 
 @NgModule({
-  declarations: [UsersListComponent, UsersViewComponent, UserCardComponent],
+  declarations: [UserListComponent, UserListItemComponent, UsersViewComponent],
   imports: [SharedModule, StaticModule, UsersRoutingModule],
 })
 export class UsersModule {}
