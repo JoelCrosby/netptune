8eb9d0e7d7edb252ae17265a08e5b12e70c69a4d
Joel Crosby <joelcrosby@live.co.uk>
2020-08-29 20:50:05 +0100

    minor improvements

--- Netptune.Repositories/BoardRepository.cs
+++ Netptune.Repositories/BoardRepository.cs
@@ -21,23 +21,25 @@ namespace Netptune.Repositories
         {
         }
 
-        public Task<List<Board>> GetBoardsInProject(int projectId, bool includeGroups = false)
+        public Task<List<Board>> GetBoardsInProject(int projectId, bool isReadonly = false, bool includeGroups = false)
         {
             var query = Entities
                 .Where(board => board.ProjectId == projectId)
-                .Where(board => !board.IsDeleted);
+                .Where(board => !board.IsDeleted)
+                .IsReadonly(isReadonly);
 
             if (!includeGroups) return query.ToListAsync();
 
             return query.Include(board => board.BoardGroups).ToListAsync();
         }
 
-        public Task<Board> GetDefaultBoardInProject(int projectId, bool includeGroups = false)
+        public Task<Board> GetDefaultBoardInProject(int projectId, bool isReadonly = false, bool includeGroups = false)
         {
             var query = Entities
                 .Where(board => board.ProjectId == projectId)
                 .Where(board => !board.IsDeleted)
-                .Where(board => board.BoardType == BoardType.Default);
+                .Where(board => board.BoardType == BoardType.Default)
+                .IsReadonly(isReadonly);
 
             if (!includeGroups) return query.FirstOrDefaultAsync();
 
