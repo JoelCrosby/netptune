b4f5f2aca6b2c61e73043b139a19c5fc8395841f
Joel Crosby <joelcrosby@live.co.uk>
2020-09-04 23:46:29 +0100

    log changes made from local storage meta reducer

--- src/app/core/meta-reducers/init-state-from-local-storage.reducer.ts
+++ src/app/core/meta-reducers/init-state-from-local-storage.reducer.ts
@@ -1,7 +1,7 @@
-import { ActionReducer, INIT, UPDATE } from '@ngrx/store';
-
-import { LocalStorageService } from '../local-storage/local-storage.service';
-import { AppState } from '../core.state';
+import { Action, ActionReducer, INIT, UPDATE } from '@ngrx/store';
+import { environment } from '@env/environment';
+import { LocalStorageService } from '@core/local-storage/local-storage.service';
+import { AppState } from '@core/core.state';
 
 export function initStateFromLocalStorage(
   reducer: ActionReducer<AppState>
@@ -9,8 +9,33 @@ export function initStateFromLocalStorage(
   return (state, action) => {
     const newState = reducer(state, action);
     if ([INIT.toString(), UPDATE.toString()].includes(action.type)) {
-      return { ...newState, ...LocalStorageService.loadInitialState() };
+      const mergedState = {
+        ...newState,
+        ...LocalStorageService.loadInitialState(),
+      };
+
+      if (!environment.production) {
+        logStorageStateChange(action, state, newState, mergedState);
+      }
+
+      return mergedState;
     }
     return newState;
   };
 }
+
+const logStorageStateChange = (
+  action: Action,
+  state: AppState,
+  newState: AppState,
+  mergedState: AppState
+) => {
+  const { type, ...payload } = action;
+
+  console.log(`[DEBUG - InitStateFromLocalStorage] action: ${type}`, {
+    payload,
+    oldState: state,
+    newState,
+    mergedState,
+  });
+};
