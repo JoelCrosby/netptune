9b4ba654b603fa6bd7b865faf2301734e8a93a78
Joel Crosby <joelcrosby@live.co.uk>
2020-09-07 19:13:20 +0100

    add card component and use in place of workspace and project cards

--- src/app/features/projects/components/project-list-item/project-list-item.component.ts
+++ src/app/features/projects/components/project-list-item/project-list-item.component.ts
@@ -1,5 +1,13 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { ProjectViewModel } from '@core/models/view-models/project-view-model';
+import { deleteProject } from '@core/store/projects/projects.actions';
+import { HeaderAction } from '@core/types/header-action';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-project-list-item',
@@ -7,6 +15,30 @@ import { ProjectViewModel } from '@core/models/view-models/project-view-model';
   styleUrls: ['./project-list-item.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProjectListItemComponent {
+export class ProjectListItemComponent implements OnInit {
   @Input() project: ProjectViewModel;
+
+  actions: HeaderAction[];
+
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    this.actions = [
+      {
+        label: 'Go To Board',
+        isLink: true,
+        icon: 'assessment',
+      },
+      {
+        label: 'Edit Project',
+        click: () => this.onEditClicked(),
+      },
+    ];
+  }
+
+  onEditClicked() {}
+
+  onDeleteClicked() {
+    this.store.dispatch(deleteProject({ project: this.project }));
+  }
 }
--- src/app/features/workspaces/components/workspace-list-item/workspace-list-item.component.ts
+++ src/app/features/workspaces/components/workspace-list-item/workspace-list-item.component.ts
@@ -7,6 +7,7 @@ import {
 import { MatDialog } from '@angular/material/dialog';
 import { Workspace } from '@core/models/workspace';
 import * as WorkspaceActions from '@core/store/workspaces/workspaces.actions';
+import { HeaderAction } from '@core/types/header-action';
 import { WorkspaceDialogComponent } from '@entry/dialogs/workspace-dialog/workspace-dialog.component';
 import { Store } from '@ngrx/store';
 
@@ -19,13 +20,33 @@ import { Store } from '@ngrx/store';
 export class WorkspaceListItemComponent implements OnInit {
   @Input() workspace: Workspace;
 
+  actions: HeaderAction[];
+
   constructor(private store: Store, private dialog: MatDialog) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.actions = [
+      {
+        label: 'Go To Projects',
+        isLink: true,
+        routerLink: ['/', this.workspace.slug],
+        icon: 'assessment',
+      },
+      {
+        label: 'Manage Users',
+        isLink: true,
+        routerLink: ['/', this.workspace.slug, 'users'],
+      },
+      {
+        label: 'Edit Workspace',
+        click: () => this.onEditClicked(),
+      },
+    ];
+  }
 
-  onEditClicked(workspace: Workspace) {
+  onEditClicked() {
     this.dialog.open(WorkspaceDialogComponent, {
-      data: workspace,
+      data: this.workspace,
       width: '720px',
     });
   }
