ab374b6268266dd12996db3c258491f9b186d8fd
Joel Crosby <joelcrosby@live.co.uk>
2020-09-14 21:29:23 +0100

    add default board link to projects list

--- src/app/core/store/workspaces/workspaces.selectors.ts
+++ src/app/core/store/workspaces/workspaces.selectors.ts
@@ -1,4 +1,5 @@
 import { selectWorkspacesFeature } from '@core/core.state';
+import { Workspace } from '@core/models/workspace';
 import { createSelector } from '@ngrx/store';
 import { adapter, WorkspacesState } from './workspaces.model';
 
@@ -27,6 +28,11 @@ export const selectWorkspacesLoaded = createSelector(
 export const selectCurrentWorkspace = createSelector(
   selectWorkspacesFeature,
   (state: WorkspacesState) => state.currentWorkspace
+);
+
+export const selectCurrentWorkspaceIdentifier = createSelector(
+  selectCurrentWorkspace,
+  (state: Workspace) => state?.slug
 );
 
 export const selectIsSlugUnique = createSelector(
