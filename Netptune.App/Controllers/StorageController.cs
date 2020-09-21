7b633878305c5512643e6c91b4fa5fb0366b87c7
Joel Crosby <joelcrosby@live.co.uk>
2020-09-21 23:52:32 +0100

    clean warnings

--- Netptune.App/Controllers/StorageController.cs
+++ Netptune.App/Controllers/StorageController.cs
@@ -1,9 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 using Netptune.Core.Encoding;
