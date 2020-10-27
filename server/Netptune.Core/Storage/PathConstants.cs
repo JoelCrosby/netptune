29b285503fdb969e6b079b7694c1ef45f9bf8390
Joel Crosby <joelcrosby@live.co.uk>
2020-10-27 04:23:01 +0000

    more editor progress

--- server/Netptune.Core/Storage/PathConstants.cs
+++ server/Netptune.Core/Storage/PathConstants.cs
@@ -3,5 +3,10 @@
     public static class PathConstants
     {
         public const string ProfilePicturePath = "user/profile/";
+
+        public static string MediaPath(string workspaceIdentifier)
+        {
+            return $"workspace/{workspaceIdentifier}/media/task/";
+        }
     }
 }
