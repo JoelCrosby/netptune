e825c58f4a6f8dd0d510d5f04509dd5e9679d325
Joel Crosby <joelcrosby@live.co.uk>
2020-12-05 00:17:16 +0000

    add ttl requirement to cache

--- server/Netptune.Services/Cache/Common/EntityCache.cs
+++ server/Netptune.Services/Cache/Common/EntityCache.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 
 using Microsoft.Extensions.Caching.Memory;
@@ -13,19 +14,38 @@ namespace Netptune.Services.Cache.Common
         protected abstract Task<TEntity> GetEntity(TKey key);
         protected abstract string GetCacheKey(TKey key);
 
-        protected EntityCache(IMemoryCache cache)
+        private readonly TimeSpan TimeToLive;
+
+        protected EntityCache(IMemoryCache cache, TimeSpan timeToLive)
         {
             Cache = cache;
+            TimeToLive = timeToLive;
         }
 
         public Task<TEntity> Get(TKey key)
         {
-            return Cache.GetOrCreateAsync(GetCacheKey(key), entry => GetEntity(key));
+            return GetOrCreateAsync(Cache, GetCacheKey(key), entry => GetEntity(key));
         }
 
         public void Remove(TKey key)
         {
             Cache.Remove(GetCacheKey(key));
+        }
+
+        private async Task<TItem> GetOrCreateAsync<TItem>(IMemoryCache cache, object key, Func<ICacheEntry, Task<TItem>> factory)
+        {
+            if (cache.TryGetValue(key, out TItem result))
+            {
+                return result;
+            }
+
+            using var entry = cache.CreateEntry(key);
+
+            result = await factory(entry).ConfigureAwait(false);
+            entry.Value = result;
+            entry.AbsoluteExpirationRelativeToNow = TimeToLive;
+
+            return result;
         }
     }
 }
