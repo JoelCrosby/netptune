45ad8cb209a505cdeb7e50905e03302630f74414
Joel Crosby <joelcrosby@live.co.uk>
2021-01-19 19:01:16 +0000

    basic form input & projkect detail page

--- client/src/app/features/projects/components/project-detail/project-detail.component.ts
+++ client/src/app/features/projects/components/project-detail/project-detail.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ProjectViewModel } from '@core/models/view-models/project-view-model';
 import { selectProjectDetail } from '@core/store/projects/projects.selectors';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-project-detail',
@@ -13,9 +15,21 @@ import { Observable } from 'rxjs';
 export class ProjectDetailComponent implements OnInit {
   project$: Observable<ProjectViewModel>;
 
-  constructor(private store: Store) {}
+  formGroup: FormGroup;
+
+  constructor(private store: Store, private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.project$ = this.store.select(selectProjectDetail);
+    this.project$ = this.store.select(selectProjectDetail).pipe(
+      tap((project) => {
+        this.formGroup = this.fb.group({
+          key: [project.key],
+          name: [project.name],
+          description: [project.description],
+          repositoryUrl: [project.repositoryUrl],
+          defaultBoard: [project.defaultBoardIdentifier],
+        });
+      })
+    );
   }
 }
--- client/src/app/static/components/form-input/form-input.component.ts
+++ client/src/app/static/components/form-input/form-input.component.ts
@@ -1,16 +1,72 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  forwardRef,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form-input',
   templateUrl: './form-input.component.html',
   styleUrls: ['./form-input.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => FormInputComponent),
+      multi: true,
+    },
+  ],
 })
-export class FormInputComponent implements OnInit {
+export class FormInputComponent implements OnInit, ControlValueAccessor {
+  @Input() label: string;
+  @Input() name: string;
+  @Input() icon: string;
+  @Input() prefix: string;
+  @Input() autocomplete = 'off';
+  @Input() placeholder = '';
+  @Input() type: 'text' | 'number' = 'text';
 
-  constructor() { }
+  @Output() submitted = new EventEmitter<string>();
 
-  ngOnInit(): void {
+  formControl = new FormControl();
+
+  onChange: (value: string) => void;
+  onTouch: () => void;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSubmit() {
+    this.submitted.emit(this.formControl.value);
   }
 
+  writeValue(obj: string) {
+    this.formControl.setValue(obj);
+  }
+
+  registerOnChange(fn: (...args: unknown[]) => unknown) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: (...args: unknown[]) => unknown) {
+    this.onTouch = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean) {
+    if (isDisabled) {
+      this.formControl.enable();
+    } else {
+      this.formControl.disable();
+    }
+  }
 }
