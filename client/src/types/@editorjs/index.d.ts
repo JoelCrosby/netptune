9a8626d1ec19d6266e1f5527be9f78eb94323452
Joel Crosby <joelcrosby@live.co.uk>
2021-01-27 01:00:38 +0000

    flatten editor js types

---
+++ client/src/types/@editorjs/index.d.ts
@@ -0,0 +1,11 @@
+declare module '@editorjs/attaches';
+declare module '@editorjs/checklist';
+declare module '@editorjs/code';
+declare module '@editorjs/embed';
+declare module '@editorjs/header';
+declare module '@editorjs/image';
+declare module '@editorjs/inline-code';
+declare module '@editorjs/link';
+declare module '@editorjs/list';
+declare module '@editorjs/marker';
+declare module '@editorjs/underline';
