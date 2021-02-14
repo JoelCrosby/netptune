06f6d6103203b6172da0f5d82e924d458c7d898c
Joel Crosby <joelcrosby@live.co.uk>
2021-02-14 02:34:49 +0000

    add activity service, repo method and controller

--- server/Netptune.Core/Repositories/IActivityLogRepository.cs
+++ server/Netptune.Core/Repositories/IActivityLogRepository.cs
@@ -1,9 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 using Netptune.Core.Entities;
+using Netptune.Core.Enums;
 using Netptune.Core.Repositories.Common;
+using Netptune.Core.ViewModels.Activity;
 
 namespace Netptune.Core.Repositories
 {
     public interface IActivityLogRepository : IWorkspaceEntityRepository<ActivityLog, int>
     {
+        Task<List<ActivityViewModel>> GetActivities(EntityType entityType, int entityId);
     }
 }
