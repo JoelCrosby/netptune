1c12b76d5b7140cadaed264520d91782db0b4117
Joel Crosby <joelcrosby@live.co.uk>
2021-06-28 01:11:09 +0100

    fix workspace list not loading

--- client/src/app/features/workspaces/components/workspace-list/workspace-list.component.ts
+++ client/src/app/features/workspaces/components/workspace-list/workspace-list.component.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Workspace } from '@core/models/workspace';
-import * as WorkspaceActions from '@core/store/workspaces/workspaces.actions';
 import { selectAllWorkspaces } from '@core/store/workspaces/workspaces.selectors';
 import { Store } from '@ngrx/store';
 
@@ -10,14 +9,10 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./workspace-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WorkspaceListComponent implements OnInit {
+export class WorkspaceListComponent {
   workspaces$ = this.store.select(selectAllWorkspaces);
 
   constructor(private store: Store) {}
-
-  ngOnInit() {
-    this.store.dispatch(WorkspaceActions.loadWorkspaces());
-  }
 
   trackById(_: number, workspace: Workspace) {
     return workspace.id;
