0b1aacececd2ed49ffbffa6a308d7407c8a1041a
Joel Crosby <joelcrosby@live.co.uk>
2022-01-06 23:17:21 +0000

    add board task export front-end

--- server/Netptune.App/Controllers/ExportController.cs
+++ server/Netptune.App/Controllers/ExportController.cs
@@ -34,4 +34,17 @@ public class ExportController : ControllerBase
 
         return File(result.Stream, result.ContentType, result.Filename);
     }
+
+    // GET: api/export/tasks/export-board
+    [HttpGet]
+    [Route("tasks/export-board/{boardId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [Produces("application/json", Type = typeof(TaskViewModel))]
+    public async Task<IActionResult> ExportBoardTasks([FromRoute] string boardId)
+    {
+        var result = await TaskExportService.ExportWorkspaceTasks();
+
+        return File(result.Stream, result.ContentType, result.Filename);
+    }
 }
