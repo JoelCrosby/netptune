0946a3c9ee7624af1fbbecbfe16e2056582b6b5b
Joel Crosby <joelcrosby@live.co.uk>
2022-03-03 23:04:29 +0000

    initial static user select component

--- client/src/app/core/models/view-models/project-task-dto.ts
+++ client/src/app/core/models/view-models/project-task-dto.ts
@@ -1,8 +1,8 @@
+import { AssigneeViewModel } from './board-view';
 import { ProjectTask } from '../project-task';
 
 export interface TaskViewModel extends ProjectTask {
-  assigneeUsername: string;
-  assigneePictureUrl?: string | null;
+  assignees: AssigneeViewModel[];
   ownerUsername: string;
   ownerPictureUrl?: string | null;
   projectName: string;
