52f6054284d49d9ee0a3ea47c1d5a92c1b911175
Joel Crosby <Joelcrosby@live.co.uk>
2022-10-25 22:38:21 +0100

    update hosted service setup

---
+++ server/Netptune.Entities/HostedDatabaseService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Netptune.Entities.Contexts;
+
+using Polly;
+using Polly.Contrib.WaitAndRetry;
+
+namespace Netptune.Entities;
+
+public class HostedDatabaseService : IHostedService
+{
+    private readonly IServiceScopeFactory ScopeFactory;
+
+    public HostedDatabaseService(IServiceScopeFactory scopeFactory)
+    {
+        ScopeFactory = scopeFactory;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        using var scope = ScopeFactory.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+        var delay = Backoff.DecorrelatedJitterBackoffV2(medianFirstRetryDelay: TimeSpan.FromSeconds(2), retryCount: 5);
+
+        await Policy
+            .Handle<Exception>()
+            .WaitAndRetry(delay)
+            .Execute(() => context.Database.EnsureCreatedAsync(cancellationToken));
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
+
