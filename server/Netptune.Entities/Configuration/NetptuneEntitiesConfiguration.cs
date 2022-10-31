385945d2bef8a02abda6a43e5e4b8c85edee0965
Joel Crosby <Joelcrosby@live.co.uk>
2022-10-31 18:43:19 +0000

    update job server startup database config

--- server/Netptune.Entities/Configuration/NetptuneEntitiesConfiguration.cs
+++ server/Netptune.Entities/Configuration/NetptuneEntitiesConfiguration.cs
@@ -27,7 +27,6 @@ public static class NetptuneEntitiesConfiguration
         services.Configure(configuration);
 
         services.AddHostedService<HostedDatabaseService>();
-
         services.AddDbContext<DataContext>(options =>
         {
             options
