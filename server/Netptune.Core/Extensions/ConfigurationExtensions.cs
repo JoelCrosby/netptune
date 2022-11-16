da9c44faaa98f0cd10cf592bff4098ef3b476bd7
Joel Crosby <joelcrosby@live.co.uk>
2022-11-16 04:57:57 +0000

    configuration cleanup

---
+++ server/Netptune.Core/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Linq;
+
+using Microsoft.Extensions.Configuration;
+
+using Netptune.Core.Utilities;
+
+namespace Netptune.Core.Extensions;
+
+public static class ConfigurationExtensions
+{
+    public static string GetNetptuneConnectionString(this IConfiguration configuration)
+    {
+        var appSettingsConString = configuration.GetConnectionString("netptune");
+        var envVar = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+        var connectionString = envVar ?? appSettingsConString;
+
+        if (connectionString is null)
+        {
+            throw new Exception("An environment variable with the key of 'DATABASE_URL' not found.");
+        }
+
+        return ConnectionStringParser.ParseConnectionString(connectionString, "netptune");
+    }
+
+    public static string GetNetptuneRedisConnectionString(this IConfiguration configuration)
+    {
+        var appSettingsConString = configuration.GetConnectionString("redis");
+        var envVar = Environment.GetEnvironmentVariable("REDIS_URL");
+
+        var connectionString = envVar ?? appSettingsConString;
+
+        if (connectionString is null)
+        {
+            throw new Exception("An environment variable with the key of 'REDIS_URL' not found.");
+        }
+
+        return ConnectionStringParser.ParseRedis(connectionString);
+    }
+
+    public static string GetEnvironmentVariable(this IConfiguration configuration, string key)
+    {
+        var result = Environment.GetEnvironmentVariable(key);
+
+        if (result is null || string.IsNullOrEmpty(result))
+        {
+            throw new Exception($"Value for environment variable '{key}' was null or empty.");
+        }
+
+        return result;
+    }
+
+    public static string[] GetCorsOrigins(this IConfiguration configuration)
+    {
+        return configuration.GetRequiredSection("CorsOrigins")
+            .AsEnumerable()
+            .Where(pair => pair.Value is { })
+            .Select(pair => pair.Value)
+            .ToArray()!;
+    }
+}
