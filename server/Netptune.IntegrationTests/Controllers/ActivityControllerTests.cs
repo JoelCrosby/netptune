fbe083daf082026a2d9509e412a996146813014f
Joel Crosby <joelcrosby@live.co.uk>
2023-03-24 22:44:53 +0000

    activity controller tests

---
+++ server/Netptune.IntegrationTests/Controllers/ActivityControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+
+using FluentAssertions;
+
+using Netptune.Core.Enums;
+using Netptune.Core.Responses.Common;
+using Netptune.Core.ViewModels.Activity;
+
+using Xunit;
+
+namespace Netptune.IntegrationTests.Controllers;
+
+public class ActivityControllerTests : IClassFixture<NetptuneApiFactory>
+{
+    private readonly HttpClient Client;
+
+    public ActivityControllerTests(NetptuneApiFactory factory)
+    {
+        Client = factory.CreateNetptuneClient();
+    }
+
+    [Fact]
+    public async Task Get_ShouldReturnCorrectly_WhenInputValid()
+    {
+        var response = await Client.GetAsync($"api/activity/{EntityType.Task}/1");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var result = await response.Content.ReadFromJsonAsync<ClientResponse<List<ActivityViewModel>>>();
+
+        result!.IsSuccess.Should().BeTrue();
+        result.Payload.Should().NotBeEmpty();
+    }
+}
