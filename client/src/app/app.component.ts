3e11fe63f9b3d21c7301ecd55e37fd64d6db433d
Joel Crosby <joelcrosby@live.co.uk>
2023-09-10 17:13:22 +0100

    minor changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,32 +1,12 @@
-import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
-import * as AuthSelectors from '@core/auth/store/auth.selectors';
-import { selectSideBarTransparent } from '@core/core.route.selectors';
-import * as LayoutSelectors from '@core/store/layout/layout.selectors';
-import { selectAllWorkspaces } from '@core/store/workspaces/workspaces.selectors';
-import { Store } from '@ngrx/store';
-import { combineLatest } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `
+    <div class="app-container">
+      <router-outlet></router-outlet>
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  @ViewChild(MatSidenav) sideNav!: MatSidenav;
-
-  sideNavOpen$ = combineLatest([
-    this.store.select(LayoutSelectors.selectSideNavOpen),
-    this.store.select(AuthSelectors.selectIsAuthenticated),
-  ]).pipe(map(([isNavOpen, isAuth]) => isAuth && isNavOpen));
-
-  workspaces$ = this.store.select(selectAllWorkspaces);
-  isMobileView$ = this.store.select(LayoutSelectors.selectIsMobileView);
-  transparentSideNav$ = this.store.select(selectSideBarTransparent);
-  sideMenuOpen$ = this.store.select(LayoutSelectors.selectSideMenuOpen);
-  user$ = this.store.select(AuthSelectors.selectCurrentUser);
-
-  constructor(private store: Store) {}
-}
+export class AppComponent {}
