bcfc466fd62fccb0995fb51a434178e213fc1e56
Joel Crosby <joelcrosby@live.co.uk>
2026-04-04 00:10:34 +0100

    update nuget packages

--- server/Netptune.Core/Utilities/UniqueIdBuilder.cs
+++ server/Netptune.Core/Utilities/UniqueIdBuilder.cs
@@ -3,15 +3,14 @@ using System;
 using HashidsNet;
 
 using shortid;
-using shortid.Configuration;
 
 namespace Netptune.Core.Utilities;
 
 public static class UniqueIdBuilder
 {
-    private static readonly GenerationOptions ShortIdOptions = new(true, false, 12);
+    private static readonly ShortIdOptions ShortIdOptions = new(true, false, 12);
 
-    public static string Generate(GenerationOptions? options = null)
+    public static string Generate(ShortIdOptions? options = null)
     {
         options ??= ShortIdOptions;
 
