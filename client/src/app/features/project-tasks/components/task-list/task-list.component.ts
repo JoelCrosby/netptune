908742b58c55e9694a430ed15c03d2f08776493a
Joel Crosby <joelcrosby@live.co.uk>
2026-04-11 20:06:18 +0100

    create static list component and use it for tasks and users

--- client/src/app/features/project-tasks/components/task-list/task-list.component.ts
+++ client/src/app/features/project-tasks/components/task-list/task-list.component.ts
@@ -1,63 +1,51 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  TrackByFunction,
+  inject,
+} from '@angular/core';
 import { selectTasks } from '@core/store/tasks/tasks.selectors';
 import { TaskViewModel } from '@core/models/view-models/project-task-dto';
 import { Store } from '@ngrx/store';
-import {
-  CdkVirtualScrollViewport,
-  CdkFixedSizeVirtualScroll,
-  CdkVirtualForOf,
-} from '@angular/cdk/scrolling';
+import { ListComponent } from '@static/components/list/list.component';
 import { TaskListItemComponent } from './task-list-item.component';
 import { TaskInlineComponent } from '../task-inline/task-inline.component';
 
 @Component({
   selector: 'app-task-list',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [
-    CdkVirtualScrollViewport,
-    CdkFixedSizeVirtualScroll,
-    CdkVirtualForOf,
-    TaskListItemComponent,
-    TaskInlineComponent,
-  ],
+  imports: [ListComponent, TaskListItemComponent, TaskInlineComponent],
   template: `
-    <div class="w-full">
-      <h4 class="text-foreground/60 mb-2 text-sm font-normal tracking-[.25px]">
-        Tasks
-      </h4>
-      <div
-        class="bg-board-group mb-5 flex h-full min-h-[196px] flex-1 flex-col overflow-hidden rounded-sm p-2.5">
-        @if (tasks()?.length) {
-          <cdk-virtual-scroll-viewport
-            class="h-[calc(100vh-262px)] min-h-16"
-            itemSize="43"
-            minBufferPx="1024"
-            maxBufferPx="2048">
-            <app-task-list-item
-              class="mb-[3px] block overflow-hidden rounded-sm"
-              *cdkVirtualFor="
-                let taskItem of tasks();
-                trackBy: trackByTask;
-                templateCacheSize: 0
-              "
-              [task]="taskItem">
-            </app-task-list-item>
-            <app-task-inline [siblings]="tasks()" />
-          </cdk-virtual-scroll-viewport>
-        } @else {
-          <app-task-inline />
-          <div class="flex justify-center">
-            <div class="flex h-full flex-col items-center justify-center">
-              <i class="far fa-compass my-8 text-[6rem]"></i>
-              <h4 class="mx-16 mb-12 text-center text-sm font-normal">
-                There are currently no tasks. Use the Create Task button above
-                to create a task
-              </h4>
-            </div>
+    <app-list
+      header="Tasks"
+      [items]="tasks()"
+      [itemSize]="43"
+      viewportClass="h-[calc(100vh-262px)] min-h-16"
+      [trackBy]="trackByTask"
+    >
+      <ng-template #item let-task>
+        <app-task-list-item
+          class="mb-[3px] block overflow-hidden rounded-sm"
+          [task]="task"
+        />
+      </ng-template>
+
+      <ng-template #listFooter>
+        <app-task-inline [siblings]="tasks()" />
+      </ng-template>
+
+      <ng-template #listEmpty>
+        <app-task-inline />
+        <div class="flex justify-center">
+          <div class="flex h-full flex-col items-center justify-center">
+            <i class="far fa-compass my-8 text-[6rem]"></i>
+            <h4 class="mx-16 mb-12 text-center text-sm font-normal">
+              There are currently no tasks. Use the Create Task button above to create a task
+            </h4>
           </div>
-        }
-      </div>
-    </div>
+        </div>
+      </ng-template>
+    </app-list>
   `,
 })
 export class TaskListComponent {
@@ -65,7 +53,5 @@ export class TaskListComponent {
 
   tasks = this.store.selectSignal(selectTasks);
 
-  trackByTask(_: number, task: TaskViewModel) {
-    return task.id;
-  }
+  readonly trackByTask: TrackByFunction<TaskViewModel> = (_, task) => task.id;
 }
---
+++ client/src/app/static/components/list/list.component.ts
@@ -0,0 +1,79 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ContentChild,
+  TemplateRef,
+  TrackByFunction,
+  input,
+} from '@angular/core';
+import {
+  CdkVirtualScrollViewport,
+  CdkFixedSizeVirtualScroll,
+  CdkVirtualForOf,
+} from '@angular/cdk/scrolling';
+import { NgTemplateOutlet } from '@angular/common';
+
+@Component({
+  selector: 'app-list',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CdkVirtualScrollViewport, CdkFixedSizeVirtualScroll, CdkVirtualForOf, NgTemplateOutlet],
+  template: `
+    <div class="w-full">
+      @if (header()) {
+        <h4 class="mb-2 text-sm font-normal tracking-[.25px] text-foreground/60">
+          {{ header() }}
+        </h4>
+      }
+      <div
+        class="bg-board-group mb-5 flex h-full min-h-[196px] flex-1 flex-col overflow-hidden rounded-sm p-2.5"
+      >
+        @if (items() !== undefined) {
+          @if (items()?.length) {
+            <cdk-virtual-scroll-viewport
+              [class]="viewportClass()"
+              [itemSize]="itemSize()"
+              minBufferPx="1024"
+              maxBufferPx="2048"
+            >
+              <ng-template
+                cdkVirtualFor
+                [cdkVirtualForOf]="items()"
+                [cdkVirtualForTemplate]="$any(itemTemplate)"
+                [cdkVirtualForTrackBy]="trackBy() ?? defaultTrackBy"
+                [cdkVirtualForTemplateCacheSize]="0"
+              ></ng-template>
+              @if (footerTemplate) {
+                <ng-container *ngTemplateOutlet="footerTemplate" />
+              }
+            </cdk-virtual-scroll-viewport>
+          } @else {
+            @if (emptyTemplate) {
+              <ng-container *ngTemplateOutlet="emptyTemplate" />
+            } @else {
+              <div class="flex flex-1 flex-col items-center justify-center opacity-60">
+                <p class="text-center text-sm">{{ emptyMessage() || 'No items to display' }}</p>
+              </div>
+            }
+          }
+        } @else {
+          <ng-content />
+        }
+      </div>
+    </div>
+  `,
+})
+export class ListComponent {
+  readonly header = input<string>();
+  readonly items = input<unknown[] | null>();
+  readonly itemSize = input(40);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  readonly trackBy = input<TrackByFunction<any>>();
+  readonly viewportClass = input('h-[calc(100vh-200px)] min-h-16');
+  readonly emptyMessage = input<string>();
+
+  @ContentChild('item') itemTemplate?: TemplateRef<unknown>;
+  @ContentChild('listFooter') footerTemplate?: TemplateRef<void>;
+  @ContentChild('listEmpty') emptyTemplate?: TemplateRef<void>;
+
+  readonly defaultTrackBy: TrackByFunction<unknown> = (_, item) => item;
+}
