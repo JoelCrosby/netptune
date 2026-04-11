d5973a0d5cd95c3ef1ccec425d45130c85b46465
Joel Crosby <joelcrosby@live.co.uk>
2026-04-11 22:29:42 +0100

    fix issue with api endpoint usage

--- client/src/app/features/profile/store/profile.service.ts
+++ client/src/app/features/profile/store/profile.service.ts
@@ -1,7 +1,6 @@
 import { AppUser } from '@core/models/appuser';
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '@env/environment';
 import { ChangePasswordRequest } from '@core/models/requests/change-password-request';
 import { ClientResponse } from '@core/models/client-response';
 import { UploadResponse } from '@core/models/upload-result';
@@ -11,28 +10,20 @@ export class ProfileService {
   private http = inject(HttpClient);
 
   get(userId: string) {
-    return this.http.get<AppUser>(
-      environment.apiEndpoint + `api/users/${userId}`
-    );
+    return this.http.get<AppUser>(`api/users/${userId}`);
   }
 
   put(user: Partial<AppUser> & { id: string }) {
-    return this.http.put<ClientResponse<AppUser>>(
-      environment.apiEndpoint + `api/users/${user.id}`,
-      user
-    );
+    return this.http.put<ClientResponse<AppUser>>(`api/users/${user.id}`, user);
   }
 
   changePassword(request: ChangePasswordRequest) {
-    return this.http.patch<ClientResponse>(
-      environment.apiEndpoint + 'api/auth/change-password',
-      request
-    );
+    return this.http.patch<ClientResponse>('api/auth/change-password', request);
   }
 
   uploadProfilePicture(data: FormData) {
     return this.http.post<ClientResponse<UploadResponse>>(
-      environment.apiEndpoint + 'api/storage/profile-picture',
+      'api/storage/profile-picture',
       data
     );
   }
