e6928c41df58f6789c85e454392b9ff656fa2aa2
Joel Crosby <joelcrosby@live.co.uk>
2026-04-12 00:38:36 +0100

    create-board-group tailwind migration

--- client/src/app/features/boards/components/create-board-group/create-board-group.component.ts
+++ client/src/app/features/boards/components/create-board-group/create-board-group.component.ts
@@ -8,8 +8,18 @@ import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-create-board-group',
-  templateUrl: './create-board-group.component.html',
-  styleUrls: ['./create-board-group.component.scss'],
+  template: `
+    <div
+      role="button"
+      tabindex="0"
+      class="flex flex-1 flex-col h-full items-center justify-center cursor-pointer m-[0.9rem] p-[0.6rem] rounded bg-background border-4 border-dashed border-border/5 text-[rgba(var(--foreground-rgb),.4)] text-sm font-medium tracking-[0.125px] transition-[background-color,margin,color] duration-200 ease-in-out hover:m-[0.4rem] hover:border-solid hover:bg-primary/[.08] hover:text-primary/80"
+      (click)="onClick()"
+      matRipple
+    >
+      <svg lucidePlus class="h-4 w-4"></svg>
+      <span class="ml-[0.4rem]">Create Group</span>
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [MatRipple, LucidePlus],
 })
