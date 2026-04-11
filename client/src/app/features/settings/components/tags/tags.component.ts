7b6dff9252a03cef72221b8c4eceefa46fa0eb83
Joel Crosby <joelcrosby@live.co.uk>
2026-04-11 19:33:08 +0100

    update tags components

--- client/src/app/features/settings/components/tags/tags.component.ts
+++ client/src/app/features/settings/components/tags/tags.component.ts
@@ -17,12 +17,60 @@ import { LucideX } from '@lucide/angular';
 import { Store } from '@ngrx/store';
 import { DocumentService } from '@static/services/document.service';
 import { TagsInputComponent } from '../tags-input/tags-input.component';
+import { StrokedButtonComponent } from '@app/static/components/button/stroked-button.component';
 
 @Component({
   selector: 'app-tags',
-  templateUrl: './tags.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [TagsInputComponent, TooltipDirective, LucideX],
+  imports: [
+    TagsInputComponent,
+    TooltipDirective,
+    LucideX,
+    StrokedButtonComponent,
+  ],
+  template: `<h3 class="font-overpass mb-4 text-[1.4rem] font-normal">Tags</h3>
+
+    <div
+      class="flex max-w-[calc(300px-.4rem)] flex-col gap-2 text-base leading-5 tracking-[.1px]">
+      @for (tag of tags(); track tag.id; let i = $index) {
+        @if (editTagIndex() === i) {
+          <app-tags-input
+            [value]="tag.name"
+            (submitted)="onEditTagSubmit($event, tag)"
+            (canceled)="onEditCanceled()">
+          </app-tags-input>
+        } @else {
+          <div
+            role="button"
+            [tabindex]="i"
+            class="group flex cursor-pointer flex-row items-center rounded-sm bg-[rgba(var(--foreground-rgb),.06)] px-[calc(.6rem+2px)] py-[calc(.4rem+2px)] text-[var(--foreground)] transition-colors duration-[240ms] ease-in-out hover:bg-[rgba(var(--foreground-rgb),.1)]"
+            (click)="onItemClicked(i)">
+            <div class="w-full">{{ tag.name }}</div>
+            <div
+              role="button"
+              aria-disabled="true"
+              class="hidden h-5 w-5 group-hover:block"
+              appTooltip="Delete Tag"
+              (click)="$event.stopPropagation(); onDeleteClicked(tag)">
+              <svg lucideX class="h-5 w-5"></svg>
+            </div>
+          </div>
+        }
+      }
+
+      <div #tagsinput>
+        @if (addTagActive()) {
+          <app-tags-input
+            (submitted)="onAddTagSubmit($event)"
+            (canceled)="onAddCanceled()">
+          </app-tags-input>
+        } @else {
+          <button app-stroked-button class="w-full" (click)="onAddTagClicked()">
+            <span class="block text-sm font-medium"> Create Tag </span>
+          </button>
+        }
+      </div>
+    </div> `,
 })
 export class TagsComponent {
   private store = inject(Store);
