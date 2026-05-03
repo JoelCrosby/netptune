bdde821c987cd197e4f4db24964ba3fb2a5387d7
Joel Crosby <joelcrosby@live.co.uk>
2026-05-04 00:34:16 +0100

    update authProvider resolver

--- client/src/app/features/auth/auth.routes.ts
+++ client/src/app/features/auth/auth.routes.ts
@@ -7,61 +7,45 @@ import { confirmEmail } from './resolvers/confirm-email.resolver';
 import { registerInvite } from './resolvers/register-invite.resolver';
 import { resetPassword } from './resolvers/reset-password.resolver';
 
+// prettier-ignore
+
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'login',
-    loadComponent: () =>
-      import('./components/login/login.component').then(
-        (m) => m.LoginComponent
-      ),
+    loadComponent: () => import('./components/login/login.component').then((m) => m.LoginComponent),
     canActivate: [loginGuard],
     canDeactivate: [canDeactivateLogin],
   },
   {
     path: 'register',
-    loadComponent: () =>
-      import('./components/register/register.component').then(
-        (m) => m.RegisterComponent
-      ),
+    loadComponent: () => import('./components/register/register.component').then((m) => m.RegisterComponent),
     resolve: {
       invite: registerInvite,
     },
   },
   {
     path: 'request-password-reset',
-    loadComponent: () =>
-      import('./components/request-password-reset/request-password-reset.component').then(
-        (m) => m.RequestPasswordResetComponent
-      ),
+    loadComponent: () => import('./components/request-password-reset/request-password-reset.component').then((m) => m.RequestPasswordResetComponent),
     canActivate: [loginGuard],
   },
   {
     path: 'reset-password',
-    loadComponent: () =>
-      import('./components/reset-password/reset-password.component').then(
-        (m) => m.ResetPasswordComponent
-      ),
+    loadComponent: () => import('./components/reset-password/reset-password.component').then((m) => m.ResetPasswordComponent),
     resolve: {
       resetPassword: resetPassword,
     },
   },
   {
     path: 'confirm',
-    loadComponent: () =>
-      import('./views/confirm-view/confirm-view.component').then(
-        (m) => m.ConfirmViewComponent
-      ),
+    loadComponent: () => import('./views/confirm-view/confirm-view.component').then((m) => m.ConfirmViewComponent),
     resolve: {
       confirmEmail: confirmEmail,
     },
   },
   {
     path: 'auth-provider-login',
-    loadComponent: () =>
-      import('./components/auth-provider/auth-provider.component').then(
-        (m) => m.AuthProviderComponent
-      ),
+    loadComponent: () => import('./components/auth-provider/auth-provider.component').then((m) => m.AuthProviderComponent),
     resolve: {
       authProviderResult: authProvider,
     },
--- client/src/app/features/auth/resolvers/auth-provider.resolver.ts
+++ client/src/app/features/auth/resolvers/auth-provider.resolver.ts
@@ -1,41 +1,54 @@
 import { inject } from '@angular/core';
 import { ActivatedRouteSnapshot, ResolveFn } from '@angular/router';
+import { AuthService } from '@app/core/auth/auth.service';
 import { loginSuccess } from '@app/core/store/auth/auth.actions';
 import { LoginResponse } from '@app/core/store/auth/auth.models';
 import { Store } from '@ngrx/store';
-import { of } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 
-export const authProvider: ResolveFn<boolean> = (
+export const authProvider: ResolveFn<boolean> = async (
   route: ActivatedRouteSnapshot
-) => {
+): Promise<boolean> => {
   const store = inject(Store);
+  const authService = inject(AuthService);
 
   const expiresValue = route.queryParamMap.get('expires');
   const email = route.queryParamMap.get('email');
   const userId = route.queryParamMap.get('userId');
 
   if (!expiresValue || !email || !userId) {
-    return of(false);
+    console.error('auth redirect query params failed: ', {
+      expiresValue,
+      email,
+      userId,
+    });
+
+    return false;
   }
 
   const expires = new Date(expiresValue);
 
   if (Number.isNaN(expires.getTime())) {
-    return of(false);
+    console.error('auth redirect expires date time failed: ', {
+      expires,
+    });
+
+    return false;
   }
 
   const displayName = route.queryParamMap.get('displayName') ?? '';
   const pictureUrl = route.queryParamMap.get('pictureUrl') ?? '';
 
+  const currentUser = await firstValueFrom(authService.currentUser());
+
   const user: LoginResponse = {
+    ...currentUser,
     expires: expiresValue,
-    email,
-    userId,
     displayName,
     pictureUrl,
   };
 
   store.dispatch(loginSuccess({ user }));
 
-  return of(true);
+  return true;
 };
