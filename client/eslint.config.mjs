38856655860f848f4068a9cdd0727e2f1121b2a8
Joel Crosby <joelcrosby@live.co.uk>
2026-05-17 01:57:09 +0100

    update eslint rules

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -49,11 +49,6 @@ export default defineConfig([
       ],
 
       '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/explicit-module-boundary-types': ['off'],
-      '@typescript-eslint/no-unsafe-member-access': ['off'],
-      '@typescript-eslint/no-unsafe-assignment': ['off'],
-      '@typescript-eslint/no-unsafe-call': ['off'],
-      '@typescript-eslint/no-unsafe-return': ['off'],
       '@typescript-eslint/no-extraneous-class': ['off'],
 
       '@typescript-eslint/no-unused-vars': [
