f97fed3315c194e78cee21b3b2700c438ffa89fa
Joel Crosby <joelcrosby@live.co.uk>
2026-05-17 02:07:27 +0100

    add task detail route

--- client/src/app/features/project-tasks/project-tasks.routes.ts
+++ client/src/app/features/project-tasks/project-tasks.routes.ts
@@ -2,6 +2,14 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: ':systemId',
+    loadComponent: () =>
+      import('./views/task-detail-page/task-detail-page.component').then(
+        (m) => m.TaskDetailPageComponent
+      ),
+    data: { title: 'Task Detail' },
+  },
+  {
     path: '**',
     loadComponent: () =>
       import('./views/project-tasks-view/project-tasks-view.component').then(
