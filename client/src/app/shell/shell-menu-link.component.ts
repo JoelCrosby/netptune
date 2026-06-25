b28ca704480049a099b76fff3599fcdd7a314f80
Joel Crosby <joelcrosby@live.co.uk>
2026-06-25 21:48:18 +0100

    update sidebar child menus

--- client/src/app/shell/shell-menu-link.component.ts
+++ client/src/app/shell/shell-menu-link.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, input, signal } from '@angular/core';
+import { Component, computed, inject, input, signal } from '@angular/core';
 import {
   LucideChevronRight,
   LucideDynamicIcon,
   LucideIconInput,
+  LucideLayoutGrid,
 } from '@lucide/angular';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TooltipDirective } from '@static/directives/tooltip.directive';
@@ -28,51 +29,62 @@ export interface ShellMenuLink {
   template: ` @if (link(); as link) {
     <div
       class="hover:bg-side-bar-active/60 my-px flex w-full items-center overflow-hidden rounded text-sm font-medium text-white/70 transition-colors select-none"
-      routerLinkActive="bg-side-bar-active text-white!">
-      <a
-        class="flex min-w-0 flex-1 cursor-pointer items-center gap-4 overflow-hidden py-2"
-        [class.justify-center]="!shell.sideNavExpanded()"
-        [class.pl-4]="shell.sideNavExpanded()"
-        [routerLink]="link.value"
-        [appTooltip]="shell.sideNavExpanded() ? '' : link.label"
-        appTooltipPosition="right">
-        @if (link.icon) {
-          <svg
-            [lucideIcon]="link.icon!"
-            class="h-4 w-4 flex-none opacity-70"></svg>
-        }
-
-        <ng-content />
-
-        @if (shell.sideNavExpanded()) {
-          <span class="truncate transition-all transition-discrete">
-            {{ link.label }}
-          </span>
-        }
-      </a>
-
-      @if (shell.sideNavExpanded() && link.children?.length) {
+      routerLinkActive="bg-side-bar-active text-white!"
+      [routerLinkActiveOptions]="{ exact: true }">
+      @if (expandable()) {
         <button
           type="button"
-          class="mr-3 flex h-6 w-6 cursor-pointer items-center justify-center rounded-sm text-white/60 transition-colors hover:bg-white/10 hover:text-white"
+          class="flex min-w-0 flex-1 cursor-pointer items-center gap-4 overflow-hidden py-2 pl-4 text-left"
           [attr.aria-expanded]="subMenuExpanded()"
           [attr.aria-label]="
             (subMenuExpanded() ? 'Collapse ' : 'Expand ') + link.label + ' menu'
           "
-          (click)="toggleSubMenu($event)">
+          (click)="toggleSubMenu()">
+          @if (link.icon) {
+            <svg
+              [lucideIcon]="link.icon!"
+              class="h-4 w-4 flex-none opacity-70"></svg>
+          }
+
+          <ng-content />
+
+          <span class="flex-1 truncate transition-all transition-discrete">
+            {{ link.label }}
+          </span>
+
           <svg
             lucideChevronRight
-            class="h-4 w-4 transition-transform"
+            class="mr-3 h-4 w-4 flex-none text-white/60 transition-transform"
             [class.rotate-90]="subMenuExpanded()"></svg>
         </button>
+      } @else {
+        <a
+          class="flex min-w-0 flex-1 cursor-pointer items-center gap-4 overflow-hidden py-2"
+          [class.justify-center]="!shell.sideNavExpanded()"
+          [class.pl-4]="shell.sideNavExpanded()"
+          [routerLink]="link.value"
+          [appTooltip]="shell.sideNavExpanded() ? '' : link.label"
+          appTooltipPosition="right">
+          @if (link.icon) {
+            <svg
+              [lucideIcon]="link.icon!"
+              class="h-4 w-4 flex-none opacity-70"></svg>
+          }
+
+          <ng-content />
+
+          @if (shell.sideNavExpanded()) {
+            <span class="truncate transition-all transition-discrete">
+              {{ link.label }}
+            </span>
+          }
+        </a>
       }
     </div>
 
-    @if (
-      shell.sideNavExpanded() && link.children?.length && subMenuExpanded()
-    ) {
-      <div class="flex flex-col">
-        @for (child of link.children; track child.value) {
+    @if (expandable() && subMenuExpanded()) {
+      <div class="mb-2 ml-6 flex flex-col border-l border-white/10 pl-2">
+        @for (child of childLinks(); track child.value) {
           <app-shell-menu-link [link]="child" />
         }
       </div>
@@ -83,11 +95,33 @@ export class ShellMenuLinkComponent {
   shell = inject(ShellService);
 
   link = input.required<ShellMenuLink>();
+  lucideLayoutGrid = LucideLayoutGrid;
   readonly subMenuExpanded = signal(false);
 
-  toggleSubMenu(event: MouseEvent) {
-    event.preventDefault();
-    event.stopPropagation();
+  /** The parent link plus an "Overview" entry pointing at the parent route. */
+  readonly childLinks = computed<ShellMenuLink[]>(() => {
+    const link = this.link();
+    const children = link.children;
+
+    if (!children?.length) {
+      return [];
+    }
+
+    return [
+      {
+        label: 'Overview',
+        value: link.value,
+        icon: this.lucideLayoutGrid,
+      },
+      ...children,
+    ];
+  });
+
+  readonly expandable = computed(
+    () => this.shell.sideNavExpanded() && this.childLinks().length > 0
+  );
+
+  toggleSubMenu() {
     this.subMenuExpanded.update((expanded) => !expanded);
   }
 }
