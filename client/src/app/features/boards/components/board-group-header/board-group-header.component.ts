e60acc21a22cb4e730ed6947ed4418b895a84b2c
Joel Crosby <joelcrosby@live.co.uk>
2026-07-03 21:42:43 +0100

    fix status and tag filter view access

--- client/src/app/features/boards/components/board-group-header/board-group-header.component.ts
+++ client/src/app/features/boards/components/board-group-header/board-group-header.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BoardGroupUsersComponent } from '@boards/components/board-group-users/board-group-users.component';
 import { BoardGroupsSearchComponent } from '@boards/components/board-groups-search/board-groups-search.component';
 import { BoardGroupsSelectionComponent } from '@boards/components/board-groups-selection/board-groups-selection.component';
 import { BoardGroupStatusComponent } from '@boards/components/board-group-status/board-group-status.component';
 import { TagFilterContainerComponent } from '@shared/components/tag-filter/tag-filter-container.component';
 import { BoardGroupHeaderSeperatorComponent } from './board-group-header-seperator.component';
+import { netptunePermissions } from '@app/core/auth/permissions';
+import { selectHasPermission } from '@app/core/store/auth/auth.selectors';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-board-group-header',
@@ -22,10 +25,24 @@ import { BoardGroupHeaderSeperatorComponent } from './board-group-header-seperat
     <app-board-group-header-seperator />
     <app-board-group-users />
     <app-board-group-header-seperator />
-    <app-tag-filter-container />
+    @if (readTags()) {
+      <app-tag-filter-container />
+    }
     <app-board-group-header-seperator />
-    <app-board-group-status />
+    @if (readStatus()) {
+      <app-board-group-status />
+    }
     <app-board-groups-selection />
   </div> `,
 })
-export class BoardGroupHeaderComponent {}
+export class BoardGroupHeaderComponent {
+  readonly store = inject(Store);
+
+  readStatus = this.store.selectSignal(
+    selectHasPermission(netptunePermissions.statuses.read)
+  );
+
+  readTags = this.store.selectSignal(
+    selectHasPermission(netptunePermissions.tags.read)
+  );
+}
--- client/src/app/features/project-tasks/components/task-list/task-list-filters.component.ts
+++ client/src/app/features/project-tasks/components/task-list/task-list-filters.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Selected } from '@core/models/selected';
 import { AssigneeViewModel } from '@core/models/view-models/board-view';
 import { TagFilterContainerComponent } from '@shared/components/tag-filter/tag-filter-container.component';
@@ -7,6 +7,9 @@ import { TaskListFilterSeparatorComponent } from './task-list-filter-separator.c
 import { TaskListSearchComponent } from './task-list-search.component';
 import { TaskListSelectionActionsComponent } from './task-list-selection-actions.component';
 import { TaskListStatusComponent } from './task-list-status.component';
+import { Store } from '@ngrx/store';
+import { selectHasPermission } from '@app/core/store/auth/auth.selectors';
+import { netptunePermissions } from '@app/core/auth/permissions';
 
 @Component({
   selector: 'app-task-list-filters',
@@ -24,13 +27,30 @@ import { TaskListStatusComponent } from './task-list-status.component';
       <app-task-list-filter-separator />
       <app-task-list-assignees [assigneeOptions]="assigneeOptions()" />
       <app-task-list-filter-separator />
-      <app-tag-filter-container />
+
+      @if (readTags()) {
+        <app-tag-filter-container />
+      }
+
       <app-task-list-filter-separator />
-      <app-task-list-status />
+
+      @if (readStatus()) {
+        <app-task-list-status />
+      }
+
       <app-task-list-selection-actions class="ml-auto" />
     </div>
   `,
 })
 export class TaskListFiltersComponent {
   readonly assigneeOptions = input<Selected<AssigneeViewModel>[] | null>(null);
+  readonly store = inject(Store);
+
+  readStatus = this.store.selectSignal(
+    selectHasPermission(netptunePermissions.statuses.read)
+  );
+
+  readTags = this.store.selectSignal(
+    selectHasPermission(netptunePermissions.tags.read)
+  );
 }
