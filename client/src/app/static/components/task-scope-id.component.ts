7a4fb4362a110f6c5408131bc1623928ad76dfd2
Joel Crosby <joelcrosby@live.co.uk>
2026-07-14 10:00:11 +0100

    task scope id changes

--- client/src/app/static/components/task-scope-id.component.ts
+++ client/src/app/static/components/task-scope-id.component.ts
@@ -3,7 +3,7 @@ import { Component, input } from '@angular/core';
 @Component({
   selector: 'app-task-scope-id',
   template: ` <div
-    class="bg-primary/10 dark:bg-primary/30 rounded-sm px-2 py-1 text-xs font-semibold whitespace-nowrap text-black select-none dark:text-white/80">
+    class="bg-primary/10 dark:bg-primary/30 w-fit rounded-sm px-2 py-1 font-mono text-xs font-semibold whitespace-nowrap text-black select-none dark:text-white/80">
     {{ id() }}
   </div>`,
   imports: [],
