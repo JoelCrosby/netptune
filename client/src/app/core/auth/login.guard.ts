73f22d1ca490022e0fef787a539ce3cb21e64928
Joel Crosby <joelcrosby@live.co.uk>
2026-07-20 23:29:20 +0100

    fix login guard

--- client/src/app/core/auth/login.guard.ts
+++ client/src/app/core/auth/login.guard.ts
@@ -1,14 +1,17 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { first, map } from 'rxjs/operators';
 import { selectIsAuthenticated } from '../store/auth/auth.selectors';
 
 export const loginGuard: CanActivateFn = () => {
   const store = inject(Store);
+  const router = inject(Router);
 
   return store.select(selectIsAuthenticated).pipe(
     first(),
-    map((isAuthenticated) => !isAuthenticated)
+    map((isAuthenticated) => {
+      return isAuthenticated ? router.createUrlTree(['/']) : true;
+    })
   );
 };
