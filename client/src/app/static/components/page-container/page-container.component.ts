df880130afbda077659bf5e98b41ca22dd1ae71d
Joel Crosby <joelcrosby@live.co.uk>
2026-07-21 01:23:43 +0100

    add a delay to page loading progress

--- client/src/app/static/components/page-container/page-container.component.ts
+++ client/src/app/static/components/page-container/page-container.component.ts
@@ -1,5 +1,9 @@
 import { Component, input } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { map, of, switchMap, timer } from 'rxjs';
 import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
+
+const progressRevealDelayMs = 200;
 
 @Component({
   selector: 'app-page-container',
@@ -8,11 +12,13 @@ import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
     class="flex flex-col"
     [class.mx-auto]="centerPage()"
     [class.w-full]="centerPage()"
-    [class.max-w-[1360px]]="centerPage()">
-    <div class="h-[0.8rem] shrink-0">
-      @if (showProgress()) {
-        <app-progress-bar mode="indeterminate" />
-      }
+    [class.max-w-[1360px]]="centerPage()"
+    [attr.aria-busy]="showProgress()">
+    <div
+      class="h-[0.8rem] shrink-0"
+      [class.invisible]="!progressVisible()"
+      [attr.aria-hidden]="progressVisible() ? null : 'true'">
+      <app-progress-bar mode="indeterminate" />
     </div>
     <div
       class="flex flex-1 flex-col px-8 max-[600px]:px-3"
@@ -29,4 +35,15 @@ export class PageContainerComponent {
   readonly marginBottom = input<boolean | null>(false);
   readonly fullHeight = input<boolean | null>(true);
   readonly centerPage = input<boolean | null>(true);
+
+  readonly progressVisible = toSignal(
+    toObservable(this.showProgress).pipe(
+      switchMap((showProgress) =>
+        showProgress
+          ? timer(progressRevealDelayMs).pipe(map(() => true))
+          : of(false)
+      )
+    ),
+    { initialValue: false }
+  );
 }
