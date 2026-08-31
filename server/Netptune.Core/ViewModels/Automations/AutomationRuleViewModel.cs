ca6c691b27ef1c2883679a766df82c88705c8cb5
Joel Crosby <joelcrosby@live.co.uk>
2026-08-31 19:26:57 +0100

    Share the automation action shape between request and view model

--- server/Netptune.Core/ViewModels/Automations/AutomationRuleViewModel.cs
+++ server/Netptune.Core/ViewModels/Automations/AutomationRuleViewModel.cs
@@ -48,79 +48,11 @@ public record AutomationTriggerViewModel
     public int? DurationDays { get; init; }
 }
 
-public record AutomationActionViewModel
+public record AutomationActionViewModel : AutomationActionFields
 {
     public int Id { get; init; }
 
-    public AutomationActionType Type { get; init; }
-
     public int SortOrder { get; init; }
-
-    public string? Message { get; init; }
-
-    public List<AutomationNotificationRecipient> Recipients { get; init; } = [];
-
-    public List<string> RecipientUserIds { get; init; } = [];
-
-    public List<WorkspaceRole> RecipientRoles { get; init; } = [];
-
-    public string? Comment { get; init; }
-
-    public string? FlagName { get; init; }
-
-    public string? FlagDescription { get; init; }
-
-    public int? StatusId { get; init; }
-
-    public TaskPriority? Priority { get; init; }
-
-    public string? TaskName { get; init; }
-
-    public string? TaskDescription { get; init; }
-
-    public bool ClearDescription { get; init; }
-
-    public string? OwnerId { get; init; }
-
-    public bool ClearOwner { get; init; }
-
-    public List<string>? AssigneeIds { get; init; }
-
-    public List<string> AddTags { get; init; } = [];
-
-    public List<string> RemoveTags { get; init; } = [];
-
-    public AutomationDateUpdate? StartDate { get; init; }
-
-    public AutomationDateUpdate? DueDate { get; init; }
-
-    public EstimateType? EstimateType { get; init; }
-
-    public decimal? EstimateValue { get; init; }
-
-    public bool ClearEstimate { get; init; }
-
-    public int? SprintId { get; init; }
-
-    public bool ClearSprint { get; init; }
-
-    public int? BoardGroupId { get; init; }
-
-    public bool CopyAssignees { get; init; }
-
-    public int? LinkRelationTypeId { get; init; }
-
-    public AutomationRelationOperation? RelationOperation { get; init; }
-
-    public AutomationRelationDirection? RelationDirection { get; init; }
-
-    public int? RelationTypeId { get; init; }
-
-    public int? RelatedTaskId { get; init; }
-
-    public int? DelayAmount { get; init; }
-
-    public AutomationDelayUnit? DelayUnit { get; init; }
 }
 
 public record AutomationManualRunViewModel
