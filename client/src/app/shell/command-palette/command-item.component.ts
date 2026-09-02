d598922e80e39358ab10c268e261d15329855a5c
Joel Crosby <joelcrosby@live.co.uk>
2026-09-02 04:32:38 +0100

    use keyboard key component in command pallet

--- client/src/app/shell/command-palette/command-item.component.ts
+++ client/src/app/shell/command-palette/command-item.component.ts
@@ -1,11 +1,12 @@
 import { Component, computed, input, output } from '@angular/core';
 import { LucideZap } from '@lucide/angular';
 import { Command } from '@core/services/command-registry.service';
+import { KeyboardKeyComponent } from '@static/components/keyboard-key/keyboard-key.component';
 import { CommandPaletteItemComponent } from './command-palette-item.component';
 
 @Component({
   selector: 'app-command-item',
-  imports: [LucideZap, CommandPaletteItemComponent],
+  imports: [LucideZap, CommandPaletteItemComponent, KeyboardKeyComponent],
   template: `
     <button
       app-command-palette-item
@@ -15,9 +16,11 @@ import { CommandPaletteItemComponent } from './command-palette-item.component';
       (mouseenter)="hover.emit()">
       <svg lucideZap class="h-4 w-4 shrink-0 opacity-50"></svg>
       <span class="flex-1 truncate text-left">{{ command().label }}</span>
-      @if (command().shortcut) {
-        <span class="text-muted ml-auto tracking-widest">
-          {{ shortcutLabel() }}
+      @if (shortcutKeys().length) {
+        <span class="ml-auto flex shrink-0 items-center gap-1">
+          @for (key of shortcutKeys(); track $index) {
+            <app-keyboard-key>{{ key }}</app-keyboard-key>
+          }
         </span>
       }
     </button>
@@ -30,9 +33,7 @@ export class CommandItemComponent {
   activate = output<Command>();
   hover = output();
 
-  readonly shortcutLabel = computed(() => {
-    return this.command()
-      .shortcut?.map((key) => key.toUpperCase())
-      .join(' ');
+  readonly shortcutKeys = computed(() => {
+    return this.command().shortcut?.map((key) => key.toUpperCase()) ?? [];
   });
 }
