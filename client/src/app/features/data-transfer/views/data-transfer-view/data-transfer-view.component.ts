6a29932617d2ef63f030cc5d9e1021a850b5f6cf
Joel Crosby <joelcrosby@live.co.uk>
2026-09-08 05:55:40 +0100

    Data view respects page width setting

--- client/src/app/features/data-transfer/views/data-transfer-view/data-transfer-view.component.ts
+++ client/src/app/features/data-transfer/views/data-transfer-view/data-transfer-view.component.ts
@@ -96,7 +96,10 @@ const ResumableStages = [
     TabGroupComponent,
   ],
   template: `
-    <app-page-container [centerPage]="true" [marginBottom]="true">
+    <app-page-container
+      followsWidthPreference
+      [centerPage]="true"
+      [marginBottom]="true">
       <app-page-header
         i18n-title="Page title for the workspace import and export page"
         title="Data" />
